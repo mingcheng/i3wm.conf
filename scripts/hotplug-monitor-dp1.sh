ba1e4c76c698d85c7f0152ee389cb372717b03f2
Ming Chen <no-reply@who-care.com>
2018-11-13 10:20:39 +0800

    :penguin: 增加 udev 相关的脚本，执行自动化

--- scripts/hotplug-monitor-dp1.sh
+++ scripts/hotplug-monitor-dp1.sh
@@ -1,46 +1,41 @@
-#!/bin/env bash
+#!/bin/sh
 
 # https://stackoverflow.com/questions/12782318/add-udev-rule-for-external-display
 # https://wiki.archlinux.org/index.php/Multihead
 
-export DISPLAY=:0.0
+sleep 3 # waiting for graph card reset
+export DISPLAY=:0
+xrandr="/usr/bin/xrandr"
 
-xrandr --output eDP1 --primary
+$xrandr --output eDP1 --primary
 
-xrandr | grep "DP1 connected"
+$xrandr | grep "DP1 connected"
 if [[ $? == 0 ]]; then
-  # is connected
-  xrandr --output DP1 --right-of eDP1 --auto --scale 2x2
+  $xrandr --output DP1 --right-of eDP1 --auto --scale $1x$1
 else
-  # not connected
-  xrandr --output DP1 --off
+  $xrandr --output DP1 --off
 fi
 
-xrandr | grep "DP2 connected"
+$xrandr | grep "DP2 connected"
 if [[ $? == 0 ]]; then
-  # is connected
-  xrandr --output DP2 --right-of eDP1 --auto --scale 2x2
+  $xrandr --output DP2 --right-of eDP1 --auto --scale $1x$1
 else
-  # not connected
-  xrandr --output DP2 --off
+  $xrandr --output DP2 --off
 fi
 
-xrandr | grep "HDMI1 connected"
+$xrandr | grep "HDMI1 connected"
 if [[ $? == 0 ]]; then
-  # is connected
-  xrandr --output HDMI1 --right-of eDP1 --auto --scale 2x2
+  $xrandr --output HDMI1 --right-of eDP1 --auto --scale $1x$1
 else
-  # not connected
-  xrandr --output HDMI1 --off
+  $xrandr --output HDMI1 --off
 fi
 
-xrandr | grep "HDMI2 connected"
+$xrandr | grep "HDMI2 connected"
 if [[ $? == 0 ]]; then
-  # is connected
-  xrandr --output HDMI2 --right-of eDP1 --auto --scale 2x2
+  $xrandr --output HDMI2 --right-of eDP1 --auto --scale $1x$1
 else
-  # not connected
-  xrandr --output HDMI2 --off
+  $xrandr --output HDMI2 --off
 fi
 
-touch /tmp/hotplug-monitor-dp1.status
+# output status to tempate file.
+$xrandr >> /tmp/hotplug-monitor-dp1.status
--- scripts/metakeymaping.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env sh
-
-setxkbmap -layout us -option ctrl:nocaps
-setxkbmap -layout us -option altwin:swap_alt_win -device 15 
